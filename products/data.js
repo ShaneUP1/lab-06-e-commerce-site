ca9d5e3bb9889ac5800fba54b896bdf510affd73
Shane Upchurch <shaneupchurch@gmail.com>
2020-10-05 17:45:02 -0500

    adds and exports data array

---
+++ products/data.js
@@ -0,0 +1,43 @@
+
+export const products = [
+    {
+        id: 'gymnastics-rings',
+        name: 'Gymnastics Rings',
+        image: 'gymnastics-rings.png',
+        description: 'Gymnastics rings have no rival in building upper-body and core strength.',
+        category: 'calisthenics',
+        price: 35.00,
+    },
+    {
+        id: 'dumbbells',
+        name: 'Dumbbells',
+        image: 'dumbbell.png',
+        description: 'Dumbbells are essential to add-in resistance training.',
+        category: 'resistance-training',
+        price: 35.00,
+    },
+    {
+        id: 'foamroller',
+        name: 'Foamroller',
+        image: 'foamroller.png',
+        description: 'Work out those knots and increase blood flow to sore areas on your rest-days.',
+        category: 'mobility',
+        price: 32.99,
+    },
+    {
+        id: 'jump-rope',
+        name: 'Jump Rope',
+        image: 'jump-rope.png',
+        description: 'A jump rope is all you need to get your heart rate up and work in some coordination and speed.',
+        category: 'cardio',
+        price: 24.99,
+    },
+    {
+        id: 'exercise-mat',
+        name: 'Exercise Mat',
+        image: 'exercise-mat.png',
+        description: 'No sense in getting dirty when you don\'t have to. Use this durable mat instead.',
+        category: 'misc',
+        price: 24.98,
+    }
+];
